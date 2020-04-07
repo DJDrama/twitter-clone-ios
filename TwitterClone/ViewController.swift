47a9c3ab16b5ad41e04ee6191e765abab2822c19
DJ <putzzang@gmail.com>
2020-04-07 12:13:12 +0900

    Initial Commit

---
+++ TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by dj on 2020/04/07.
+//  Copyright © 2020 DJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
