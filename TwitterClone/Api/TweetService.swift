5468da09a7df36636904ef14148f40f35682bf3f
DJ <putzzang@gmail.com>
2020-04-15 23:40:34 +0900

    Sending Tweets

---
+++ TwitterClone/Api/TweetService.swift
@@ -0,0 +1,25 @@
+//
+//  TweetService.swift
+//  TwitterClone
+//
+//  Created by dj on 2020/04/15.
+//  Copyright © 2020 DJ. All rights reserved.
+//
+
+import Firebase
+
+struct TweetService {
+    static let shared = TweetService()
+    
+    func uploadTweet(caption: String, completion: @escaping(Error?, DatabaseReference) -> Void){
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let values = ["uid" : uid,
+                      "timestamp" : Int(NSDate().timeIntervalSince1970),
+                      "likes": 0,
+                      "retweets": 0,
+                      "caption": caption] as [String: Any]
+        
+        REF_TWEETS.childByAutoId().updateChildValues(values, withCompletionBlock: completion)
+    }
+}
