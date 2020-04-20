e2b1cca0f76c4270749a20112846d5a02785cdf8
DJ <putzzang@gmail.com>
2020-04-21 00:16:18 +0900

    making user profile

---
+++ TwitterClone/Controller/ProfileController.swift
@@ -0,0 +1,73 @@
+//
+//  ProfileController.swift
+//  TwitterClone
+//
+//  Created by dj on 2020/04/20.
+//  Copyright © 2020 DJ. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "TweetCell"
+private let headerIdentifier = "ProfileHeader"
+class ProfileController: UICollectionViewController{
+    // MARK: - Properties
+
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureCollectionView()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.navigationBar.barStyle = .black //making status bar to white
+        navigationController?.navigationBar.isHidden=true
+    }
+
+    
+    // MARK: - Helpers
+    func configureCollectionView() {
+        collectionView.backgroundColor = .white
+        collectionView.contentInsetAdjustmentBehavior = .never // covering top safe area!
+        
+        collectionView.register(TweetCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.register(ProfileHeader.self,
+                                forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader,
+                                withReuseIdentifier: headerIdentifier)
+    }
+
+}
+
+
+// MARK: - UICollectionViewDelegate
+extension ProfileController {
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerIdentifier, for: indexPath) as! ProfileHeader
+        return header
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension ProfileController {
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TweetCell
+        
+        return cell
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension ProfileController : UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: view.frame.width, height: 300)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width, height: 120)
+    }
+}
---
+++ TwitterClone/Model/Tweet.swift
@@ -0,0 +1,34 @@
+//
+//  Tweet.swift
+//  TwitterClone
+//
+//  Created by dj on 2020/04/16.
+//  Copyright © 2020 DJ. All rights reserved.
+//
+
+import Foundation
+
+struct Tweet {
+    let caption: String
+    let tweetID: String
+    let uid: String
+    let likes: Int
+    var timestamp: Date!
+    let retweetCount: Int
+    
+    let user: User
+    
+    init(user: User, tweetID: String, dictionary: [String: Any]){
+        self.user = user
+        
+        self.tweetID = tweetID
+        self.caption = dictionary["caption"] as? String ?? ""
+        self.uid = dictionary["uid"] as? String ?? ""
+        self.likes = dictionary["likes"] as? Int ?? 0
+        self.retweetCount = dictionary["retweets"] as? Int ?? 0
+        
+        if let timestamp = dictionary["timestamp"] as? Double {
+            self.timestamp = Date(timeIntervalSince1970: timestamp)
+        }
+    }
+}
